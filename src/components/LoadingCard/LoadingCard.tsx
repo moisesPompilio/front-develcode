2f66d2fdd99c053eafa95e6fa654b57b08461b4a
moisesPompilio <moisesjtmed@gmail.com>
2023-06-25 22:26:40 -0300

    feat[Componentes exibição]: faz a indicação que esta esperando a requicição da api ser feita

---
+++ src/components/LoadingCard/LoadingCard.tsx
@@ -0,0 +1,13 @@
+import './LoadingCard.scss';
+
+export function LoadingCard() {
+  return (
+    <div className="loading-card">
+      <div className="loading-card-image"> </div>
+      <div className="loading-card-details">
+        <div className="loading-card-field"> </div>
+        <div className="loading-card-field"> </div>
+      </div>
+    </div>
+  );
+}
